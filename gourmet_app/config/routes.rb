541766e149de8fcc3da31e71dfea29e6c23478bd
shinji_Note <shinzi7280.18@gmail.com>
2022-06-20 16:16:29 +0900

    修正

--- gourmet_app/config/routes.rb
+++ gourmet_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => "shop#index"
+  get '/shop/index' => 'shop#index'
   resources :users, only: [:show, :edit, :update]
   resources :shop, only: [:show] do
     resource :favorites, only: [:create, :destroy]
